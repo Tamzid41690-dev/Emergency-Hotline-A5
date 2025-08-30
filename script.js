bc96266726632e1077444754f1e7501b78e99ca7
Tamzid41690-dev <tasnimahmed41690@gmail.com>
2025-08-30 21:53:01 +0600

    js update

---
+++ script.js
@@ -0,0 +1,49 @@
+
+//heart icon and heart count //
+
+document.getElementById('hi1').addEventListener('click', function() {
+   const countElement = document.getElementById('heart-count').innerText;
+   const count = parseInt(countElement);
+    document.getElementById('heart-count').innerText = count + 1;
+});
+document.getElementById('hi2').addEventListener('click', function() {
+   const countElement = document.getElementById('heart-count').innerText;
+   const count = parseInt(countElement);
+    document.getElementById('heart-count').innerText = count + 1;
+});
+document.getElementById('hi3').addEventListener('click', function() {
+   const countElement = document.getElementById('heart-count').innerText;
+   const count = parseInt(countElement);
+    document.getElementById('heart-count').innerText = count + 1;
+});
+document.getElementById('hi4').addEventListener('click', function() {
+   const countElement = document.getElementById('heart-count').innerText;
+   const count = parseInt(countElement);
+    document.getElementById('heart-count').innerText = count + 1;
+});
+document.getElementById('hi5').addEventListener('click', function() {
+   const countElement = document.getElementById('heart-count').innerText;
+   const count = parseInt(countElement);
+    document.getElementById('heart-count').innerText = count + 1;
+});
+document.getElementById('hi6').addEventListener('click', function() {
+   const countElement = document.getElementById('heart-count').innerText;
+   const count = parseInt(countElement);
+    document.getElementById('heart-count').innerText = count + 1;
+});
+document.getElementById('hi7').addEventListener('click', function() {
+   const countElement = document.getElementById('heart-count').innerText;
+   const count = parseInt(countElement);
+    document.getElementById('heart-count').innerText = count + 1;
+});
+document.getElementById('hi8').addEventListener('click', function() {
+   const countElement = document.getElementById('heart-count').innerText;
+   const count = parseInt(countElement);
+    document.getElementById('heart-count').innerText = count + 1;
+});
+document.getElementById('hi9').addEventListener('click', function() {
+   const countElement = document.getElementById('heart-count').innerText;
+   const count = parseInt(countElement);
+    document.getElementById('heart-count').innerText = count + 1;
+});
+//heart icon and heart count ends here //
